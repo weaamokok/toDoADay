2461ccdb345644f6a275574238d0982dd72cd593
weaamokok <wemookok12@gmail.com>
2022-04-05 01:09:02 +0200

    setting files up
    flutter files

---
+++ to_do_a_day/lib/screens/task_screen.dart
@@ -0,0 +1,11 @@
+import 'dart:io';
+import 'package:flutter/material.dart';
+
+class TasksScreen extends StatelessWidget {
+  const TasksScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
