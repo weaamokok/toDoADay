ccf54bae5f726e4f3fa89b7f8a423def53106985
weaamokok <wemookok12@gmail.com>
2022-04-05 22:41:21 +0200

    Task Screen part 1
    task screen needs some working on the white container that holds the todos

---
+++ to_do_a_day/lib/elements/const.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+Color conPrimaryY = Color(0xffF3C623);
+TextStyle conToday = TextStyle(
+    color: Colors.white,
+    fontFamily: 'ComicsB',
+    fontSize: 31,
+    fontWeight: FontWeight.w600);
+TextStyle conTodaysDate = TextStyle(
+    color: Colors.white,
+    fontFamily: 'Comics',
+    fontSize: 13,
+    fontWeight: FontWeight.normal);
--- to_do_a_day/lib/screens/task_screen.dart
+++ to_do_a_day/lib/screens/task_screen.dart
@@ -1,12 +1,18 @@
+// ignore_for_file: prefer_const_literals_to_create_immutables
+
 import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:intl/intl.dart';
+import '../elements/const.dart';
 
+//TODO find a way to make the white container to take the remaining space of the screen
 class TasksScreen extends StatelessWidget {
-  const TasksScreen({Key? key}) : super(key: key);
-
+  DateTime date = new DateTime.now();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: conPrimaryY,
       appBar: AppBar(
         leading: Icon(
           Icons.menu_rounded,
@@ -18,12 +24,110 @@ class TasksScreen extends StatelessWidget {
             Icons.help_rounded,
             color: Colors.white,
             size: 30,
-          )
+          ),
+          SizedBox(
+            width: 10,
+          ),
         ],
         elevation: 0,
-        backgroundColor: Color(0xffF3C623),
+        backgroundColor: conPrimaryY,
       ),
-      body: Container(),
+      body: Container(
+        height: MediaQuery.of(context).size.height,
+        width: double.infinity,
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                width: double.infinity,
+                margin: EdgeInsets.only(top: 20, left: 50),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Today',
+                      style: conToday,
+                    ),
+                    SizedBox(
+                      height: 9,
+                    ),
+                    Text(
+                      DateFormat('EEEE').format(date).substring(
+                              0, 3) + //date fromat is from intl package
+                          ',' +
+                          DateFormat('d').format(date) +
+                          ' ' +
+                          DateFormat('MMMM').format(date),
+                      style: conTodaysDate,
+                    ),
+                    //today's date
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: 25,
+              ),
+              Container(
+                padding: EdgeInsets.all(50),
+                width: double.infinity,
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius:
+                        BorderRadius.only(topLeft: Radius.circular(50))),
+                child: SingleChildScrollView(
+                  child: Column(
+                    children: [
+                      Center(
+                        child: Column(
+                          children: [
+                            SizedBox(
+                              height: 50,
+                            ),
+                            SizedBox(
+                              width: 209,
+                              height: 209,
+                              child: Image(
+                                image: AssetImage(
+                                    'images/Stuck at Home - To Do List.png'),
+                              ),
+                            ),
+                            SizedBox(
+                              height: 10,
+                            ),
+                            Text(
+                              'add your to-dos for today,\n remember your to-dos will be archived when the day is over',
+                              style: TextStyle(
+                                  fontFamily: 'Comics',
+                                  fontSize: 16,
+                                  color: Color(0xff2A3642).withOpacity(.2)),
+                              textAlign: TextAlign.center,
+                            ),
+                          ],
+                        ),
+                      ),
+                      SizedBox(
+                        height: 10,
+                        // height: MediaQuery.of(context).size.height * .158,
+                      ),
+                      Align(
+                        alignment: Alignment.bottomRight,
+                        child: FloatingActionButton(
+                            backgroundColor: Color(0xff127681),
+                            child: Icon(
+                              Icons.add_rounded,
+                              size: 35,
+                            ),
+                            onPressed: () {}),
+                      )
+                    ],
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
